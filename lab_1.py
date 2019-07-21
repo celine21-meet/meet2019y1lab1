90ea536a8c4fd2dd227da38f047895084b414d72
celine21-meet <celine21@meet.mit.edu>
2019-07-21 11:26:25 -0400

    Stopping here

---
+++ lab_1.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+t.circle(100,360,50)
+t.penup()
+t.color("red")
+t.goto(250,0)
+t.pendown()
+t.circle(100,360,50)
+t.penup()
+t.color("blue")
+t.right(90)
+t.forward(550)
+t.pendown()
+t.circle(100,360,50)
+
